5ce42124e9abb129cb762e055ced6e6820bdaa9b
Anton Bugrin <abugrin@yandex.ru>
2024-10-10 11:41:34 +0300

    Initial commit

--- gpt.py
+++ gpt.py
@@ -11,19 +11,27 @@ yb = MessengerBot(os.getenv('BOT_KEY'), log_level=logging.INFO)
 ml = YCloudML(folder_id=os.getenv('GPT_FOLDER'), auth=os.getenv('GPT_API_KEY'))
 model = ml.models.completions('yandexgpt').langchain(model_type="chat", timeout=60)
 
-langchain = []
+lang_chains = {}
 
 @yb.add_handler(command='/clear')
 def clear_context(update: Update):
-    global langchain
-    langchain.clear()
+    global lang_chains
+    lang_chains.pop(f'{update.from_m.from_id}')
+    yb.send_message(f'Context cleared for user {update.from_m.login}', update)
 
 @yb.add_handler(any=True)
 def process_any(update: Update):
-    global langchain
-    langchain.append(HumanMessage(content=update.text))
-    ai_message: AIMessage = model.invoke(langchain)
-    langchain.append(ai_message)
+    global lang_chains
+    lang_chain = lang_chains.get(f'{update.from_m.from_id}')
+
+    if not lang_chain:
+        lang_chain = {f'{update.from_m.from_id}': [HumanMessage(content=update.text)]}
+        lang_chains.update(lang_chain)
+    else:
+        lang_chains[f'{update.from_m.from_id}'].append(HumanMessage(content=update.text))
+
+    ai_message: AIMessage = model.invoke(lang_chains[f'{update.from_m.from_id}'])
+    lang_chains[f'{update.from_m.from_id}'].append(ai_message)
     yb.send_message(ai_message.content, update)
 
 
